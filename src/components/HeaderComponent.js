6a638303b3dfa520bb2aeff4142412e33624353d
ishant <ishantchauhan710@gmail.com>
2022-02-20 23:29:40 +0530

    Header Layout Designed

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div className='header'>
+        <div class="button-drawer-menu"><i class="material-icons button-drawer-menu-icon">menu</i></div>
+        <div class="logo">News Blender</div>
+    </div>
+  )
+}
+
+export default HeaderComponent;
