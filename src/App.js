4b3bcd81dd96aa00e85638b1ecac834e6b2cfbc8
ishant <ishantchauhan710@gmail.com>
2022-02-21 12:48:54 +0530

    News Card Designed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import HeaderComponent from './components/HeaderComponent.js';
+import NewsComponent from './components/NewsComponent.js';
+
 
 
 function App() {
   return (
     <div className="App">
       <HeaderComponent />
+      <NewsComponent />
     </div>
   );
 }
