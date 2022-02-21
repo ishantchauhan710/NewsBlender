f6fc1e82a0349a121c45fc317fb6ab408a7a1ae1
ishant <ishantchauhan710@gmail.com>
2022-02-22 00:05:23 +0530

    Date Formatted

--- src/components/NewsCardComponent.js
+++ src/components/NewsCardComponent.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 const NewsCardComponent = ({news}) => {
+
+  const date = Date(news.publishedAt).toString().split(" ");
+  const hour = parseInt(date[4].substring(0,2));
+  const time = hour > 12 ? true : false;
+
+
   return (
     <div className='news-card'>
 
@@ -10,7 +16,7 @@ const NewsCardComponent = ({news}) => {
             
             <div className='news-content'>
                 <span className='news-title'>{news.title}</span>
-                <span className='news-author'>By {news.author? news.author : "unknown"} | {news.publishedAt}</span>
+                <span className='news-author'>By {news.author? news.author : "unknown"} | {date[0]} , {date[2]} {date[1]} {date[3]} at {time ? `${hour-12}:${date[4].substring(3,5)} pm` : `${hour}:${date[4].substring(3,5)} am`}</span>
                 <span className='news-description'>{news.description}</span>
                 <span className='news-url'><a href={news.url}>Read More</a></span>
             </div>
