8dd32c400ae11d6c335bf45e94f3724f45f11a30
ishant <ishantchauhan710@gmail.com>
2022-02-21 12:01:34 +0530

    HeaderComponent Content Changed

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -5,7 +5,7 @@ const HeaderComponent = () => {
   return (
     <div className='header'>
         <div class="button-drawer-menu"><DrawerComponent /></div>
-        <div class="logo">News Blender</div>
+        <div class="logo">News Blender App</div>
     </div>
   )
 }
